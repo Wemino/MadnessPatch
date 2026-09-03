a5e1b1041391411e029a1e891420f8410cb0e5c1
Wem <20968054+Wemino@users.noreply.github.com>
2026-09-03 13:54:49 +0200

    Update SDL to 3.4.16

--- include/SDL3/SDL_revision.h
+++ include/SDL3/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "SDL-release-3.4.12-0-gf87239e71 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-release-3.4.16-0-gfa2c02bb6 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-release-3.4.12-0-gf87239e71"
+#define SDL_REVISION "SDL-release-3.4.16-0-gfa2c02bb6"
 #endif
 
 #endif /* SDL_revision_h_ */
